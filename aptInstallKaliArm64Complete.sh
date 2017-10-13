66f4b18a18a94cf0cd603f0c638f5836b43bc6a8
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-12 21:50:57 -0300

    Create aptInstallKaliArm64Complete.sh

---
+++ aptInstallKaliArm64Complete.sh
@@ -0,0 +1,23 @@
+echo "[+]Installing Kali Arm 64 minimal version.........."
+echo
+apt install proot wget tar -y
+echo
+echo "installed successfully.... :D"
+echo
+echo "Now Kali Nethunter Is Installing......."
+echo
+wget https://build.nethunter.com/kalifs/kalifs-latest/kalifs-arm64-minimal.tar.xz
+echo
+proot --link2symlink tar -xf kalifs-arm64-minimal.tar.xz
+cd kali-arm64 && echo "nameserver 8.8.8.8" > etc/resolv.conf
+
+cd ../ && echo "proot --link2symlink -0 -r kali-arm64 -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
+
+chmod 777 startkali.sh && termux-fix-shebang startkali.sh
+
+cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+chmod 700 /data/data/com.termux/files/usr/bin/kali
+echo
+echo 
+echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
+;;
