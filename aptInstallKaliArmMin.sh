dece4c74da55261be208d8f1cd940d96b0d54292
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-28 11:36:40 -0200

    Update aptInstallKaliArmMin.sh

--- aptInstallKaliArmMin.sh
+++ aptInstallKaliArmMin.sh
@@ -2,7 +2,7 @@ echo "[+]Installing Kali Arm 32 Minimal Version..."
 echo
 apt install proot wget tar -y
 echo
-echo "installed successfully.... :D"
+echo "[...]installed successfully...."
 echo
 echo "Now Kali Nethunter Is Installing......."
 echo
@@ -11,13 +11,12 @@ echo
 proot --link2symlink tar -xf kalifs-armhf-minimal.tar.xz
 cd kali-armhf && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
-cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
+cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /data/data/com.termux/files/home/kali-armhf/sys/ -b /proc/ -b /data/data/com.termux/files/home/kali-armhf/root -b /data/data/com.termux/files/home/kali-armhf/system -b /data/data/com.termux/files/home/kali-armhf/mnt /usr/bin/env -i HOME=/data/data/com.termux/files/home/kali-armhf/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > kali.sh
 
-chmod 777 startkali.sh && termux-fix-shebang startkali.sh
+chmod 777 kali.sh && termux-fix-shebang kali.sh
 
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+cat kali.sh > /data/data/com.termux/files/usr/bin/kali
 chmod 700 /data/data/com.termux/files/usr/bin/kali
 echo
 echo 
-echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
-;;
+echo "[...]Start Kali with: ./kali.sh or kali"
