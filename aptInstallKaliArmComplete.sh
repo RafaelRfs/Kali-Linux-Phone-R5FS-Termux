a0d1c4fee1ce855ea8ad071c71b456a94a72b11a
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 12:41:27 -0200

    Update aptInstallKaliArmComplete.sh

--- aptInstallKaliArmComplete.sh
+++ aptInstallKaliArmComplete.sh
@@ -1,22 +1,12 @@
 echo "[+]Installing Kali Arm 32 bit full version..."
-echo
 apt install proot wget tar -y
-echo
 echo "installed successfully.... :D"
-echo
-echo "Now Kali Nethunter Is Installing......."
-echo
+echo "Now Kali Nethunter Phone Hacker Termux Is Installing......."
 wget https://build.nethunter.com/kalifs/kalifs-latest/kalifs-armhf-full.tar.xz
-echo
 proot --link2symlink tar -xf kalifs-armhf-full.tar.xz
 cd kali-armhf && echo "nameserver 8.8.8.8" > etc/resolv.conf
-
-cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
-
-chmod 777 startkali.sh && termux-fix-shebang startkali.sh
-
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /data/data/com.termux/files/home/kali-armhf/sys/ -b /proc/ -b /data/data/com.termux/files/home/kali-armhf/root -b /data/data/com.termux/files/home/kali-armhf/system -b /data/data/com.termux/files/home/kali-armhf/mnt /usr/bin/env -i HOME=/data/data/com.termux/files/home/kali-armhf/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > kali.sh
+chmod 777 kali.sh && termux-fix-shebang kali.sh
+cat kali.sh > /data/data/com.termux/files/usr/bin/kali
 chmod 700 /data/data/com.termux/files/usr/bin/kali
-echo
-echo 
-echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
+echo "[...]Start Kali with: ./kali.sh or kali"
