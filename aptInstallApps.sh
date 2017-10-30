425f25dd009b3190ddad621a63da6a95d8339868
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 12:53:09 -0200

    Update aptInstallApps.sh

--- aptInstallApps.sh
+++ aptInstallApps.sh
@@ -1,6 +1,7 @@
+echo "[...]Installing Apps..."
 apt update
 apt --fix-broken install
 apt -y upgrade
 apt install -y  net-tools php php-curl python python-pip python3-pip postgresql postgresql-contrib
-apt install metasploit-framework
 pip install scapy
+apt install metasploit-framework
