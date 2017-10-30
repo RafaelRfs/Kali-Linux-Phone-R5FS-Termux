819e17f5fd615ac8d5576cf1b712e46782aa8693
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 12:59:39 -0200

    Update aptInstallApps.sh

--- aptInstallApps.sh
+++ aptInstallApps.sh
@@ -3,5 +3,6 @@ apt update
 apt --fix-broken install
 apt -y upgrade
 apt install -y  net-tools php php-curl python python-pip python3-pip postgresql postgresql-contrib iptables ettercap-text-only sslstrip
-pip install scapy
+pip install python-nmap sqlmap colorama PyX requests setuptools urllib3 scapy wifi
+pip3 install BeautifulSoup colorama pycparser six urllib3 wifi youtube-dl
 apt install metasploit-framework
