f2f302517882bac62113cae10666e6a9e9f90d2f
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 12:52:07 -0200

    Update aptInstallApps.sh

--- aptInstallApps.sh
+++ aptInstallApps.sh
@@ -1,5 +1,6 @@
-apt update 
-apt install -y  apache2  git ruby ruby-dev nmap netcat net-tools make php php-pgsql php-curl figlet ecj
-apt install -y  wget nano hydra sqlite libsqlite-dev postgresql postgresql-contrib 
-apt install -y  cmatrix libffi-dev glib gnupg gnupg2 libxslt-dev libtool libxml2-dev libxslt-dev libgrpc-dev pkg-config python python-dev python3 python3-pip
-apt install -y  pinetutils findutils readline-dev  openssl tcpdump unzip openssl-dev zip w3m 
+apt update
+apt --fix-broken install
+apt -y upgrade
+apt install -y  net-tools php php-curl python python-pip python3-pip postgresql postgresql-contrib
+apt install metasploit-framework
+pip install scapy
