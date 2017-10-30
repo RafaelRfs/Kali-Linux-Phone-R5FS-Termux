739ca91015515253cb1d4a68f2749374d0409adb
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 13:05:02 -0200

    Update and rename aptInstallPhoneHacker.sh to Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,18 @@
+echo "##############################################"
+echo " Phone Hacker Lv 2 Kali Nethunter R5FS ~> Termux"
+echo "##############################################"
+echo "______________________________________________"
+echo "[+]Setting the Termux Kali..."
+echo "______________________________________________"
+apt update
+apt install -y dos2unix net-tools
+dos2unix *.sh
+chmod 777 -R *
+./aptInstallBin.sh
+./aptInstallApps.sh
+echo "[...]Installation Complete..."
+screenfet
+sleep 5
+echo "##############################################"
+echo "[+]PHONE-HACKER LV.2 Success"
+echo "##############################################"
