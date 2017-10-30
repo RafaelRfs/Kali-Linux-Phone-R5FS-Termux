e4594d4b584da2f325f0a5ad87569489b96440a1
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-30 20:45:40 -0200

    Update aptInstallBin.sh

--- aptInstallBin.sh
+++ aptInstallBin.sh
@@ -1,12 +1,12 @@
 bin_dir=/usr/bin/
 echo "[+]Installing System binaries..."
 chmod 777 -R *
-sudo mv Client $bin_dir
-sudo mv Server $bin_dir
-sudo mv mit $bin_dir
-sudo mv network $bin_dir
-sudo mv network-nmap $bin_dir
-sudo mv verser $bin_dir
-sudo mv screenfet $bin_dir
-sudo mv ngrok $bin_dir
+mv Client $bin_dir
+mv Server $bin_dir
+mv mit $bin_dir
+mv network $bin_dir
+mv network-nmap $bin_dir
+mv verser $bin_dir
+mv screenfet $bin_dir
+mv ngrok $bin_dir
 echo "[...]Complete..."
