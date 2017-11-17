2d72488d37f337ac1aef5b0bdfe693fb790a330f
R5fs <RafaelRfs@users.noreply.github.com>
2017-11-17 16:18:45 -0200

    Update aptInstallBin.sh

--- aptInstallBin.sh
+++ aptInstallBin.sh
@@ -1,6 +1,10 @@
 bin_dir=/usr/bin/
 echo "[+]Installing System binaries..."
 chmod 777 -R *
+cat iptab.sh > ${bin_dir}iptab
+chmod 777 ${bin_dir}iptab
+cat setEtr.sh > ${bin_dir}setEtr
+chmod 777 ${bin_dir}setEtr
 mv Client $bin_dir
 mv Server $bin_dir
 mv mit $bin_dir
