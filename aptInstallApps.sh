01313db9b24b345f029467722a8e2b1e044f22e3
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-11-29 12:55:30 -0200

    Update aptInstallApps.sh

--- aptInstallApps.sh
+++ aptInstallApps.sh
@@ -4,7 +4,7 @@ dpkg --add-architecture i386
 apt update
 apt --fix-broken install
 apt -y upgrade
-apt install -y  net-tools fail2ban aide php sudo php-curl python python-pip python3-pip postgresql postgresql-contrib ftp-ssl iptables ettercap-text-only sslstrip dsniff bluetooth
+apt install -y  net-tools zsh fail2ban aide php sudo php-curl python python-pip python3-pip postgresql postgresql-contrib ftp-ssl iptables ettercap-text-only sslstrip dsniff bluetooth
 pip install scapy
 pip install python-nmap sqlmap colorama PyX requests setuptools urllib3 wifi
 pip3 install BeautifulSoup colorama pycparser six urllib3 wifi youtube-dl
