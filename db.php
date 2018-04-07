3a432283844c805349453a9b49d35fb143db6070
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2018-04-07 00:33:41 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,25 +1,25 @@
 <?php
-define('USER', 'postgres');
+define('USERNAME', 'postgres');
 define('PASS','postgres');
 define('HOST','localhost');
 define('DBNAME','postgres');
 define('TYPEBD','pgsql');
 
 class Db{
-	
 private $conn, $table = 'users' ;
 
 function __construct(){
-	$this->getConn();
+		$this->getConn();
 	}
 	
 function getConn(){
 	if(is_null($this->conn)){
-	$this->conn =  new PDO(TYPEBD.':host='.HOST.';dbname='.DBNAME, USER,PASS, array(PDO::ATTR_PERSISTENT => true));	
+	$this->conn =  new PDO(TYPEBD.':host='.HOST.';dbname='.DBNAME, USERNAME,PASS, array(PDO::ATTR_PERSISTENT => true));	
 	}
 	return $this->conn;
 }
-	
+
+
 public function setTable($table){
 	$this->table = $table;
 	}
@@ -56,14 +56,16 @@ function prepareSql($pdo, $dta,$dx){
 	if($count == $count2){
       for($i = 0; $i < $count ; $i++){
 		  $v = $i + 1 ;
-		  $pdo = $this->getVarTp($v,$dta[$dx['ind'][$i]], $pdo);  
+		  $pdo = $this->getVarTp($v,$dta[$dx['ind'][$i]], $pdo);
+		  
 		}
 	$pdo->execute();  
 	}
-	}	
-	
+	}
+		
 function getVarTp($indice,  $value, $pdo){
 	$tp_data = gettype($value);
+	
 	switch($tp_data){
 		case "integer":
 		$pdo->bindValue($indice, $value, PDO::PARAM_INT );
@@ -72,7 +74,7 @@ function getVarTp($indice,  $value, $pdo){
 		case "string":
 		$pdo->bindValue($indice, $value, PDO::PARAM_STR );
 		break;
-			
+		
 		case "NULL":
 		$pdo->bindValue($indice, $value, PDO::PARAM_NULL );
 		break;
@@ -90,14 +92,17 @@ function getVarTp($indice,  $value, $pdo){
 		break;
 	
 		default:
-		$pdo->bindValue($indice, $value, PDO::PARAM_STR );		
+		$pdo->bindValue($indice, $value, PDO::PARAM_STR );	
+		
 	}
 	return $pdo;
 }	
-		
+	
+	
 function Prepare($sql,$arg = '',$arg2 = '',$arg3='',$arg4='',$arg5 = ''){
 		$data = array();
-		$dados = $this->getConn()->prepare($sql);	
+		$dados = $this->getConn()->prepare($sql);
+				
 		if(trim($arg) <> '' ){  $dados->bindValue(1,$arg,PDO::PARAM_STR);     }
 		if(trim($arg2) <> '' ){  $dados->bindValue(2,$arg2,PDO::PARAM_STR);     }
 		if(trim($arg3) <> '' ){  $dados->bindValue(3,$arg3,PDO::PARAM_STR);     }
@@ -113,15 +118,15 @@ function Prepare($sql,$arg = '',$arg2 = '',$arg3='',$arg4='',$arg5 = ''){
 public function Create($data,$table = ''){
 	$tab = trim($table) == '' ? $this->table : $table;
 	$dt = $this->getStr($data);
-	$sql = 'INSERT INTO '.$tab.'('.$dt['str'].') Values('.$dt['qst'].')';
+	$sql = "INSERT INTO ".$tab."(".$dt['str'].") Values(".$dt['qst'].")";
 	$pdo = $this->getConn()->prepare($sql);
 	$this->prepareSql($pdo,$data,$dt);
     }
 
-public function Read($where = '', $table = ''){
+public function Read($where = '', $table = '', $limit = "",$orderby = ""){
 	$tab = trim($table) == '' ? $this->table : $table;
-	$sql = 'SELECT * FROM '.$tab;
-	$sql = strip_tags(trim($where == ''))? $sql : $sql.' WHERE '.$where;
+	$sql = "SELECT * FROM ".$tab;
+	$sql = strip_tags(trim($where == ''))? $sql : $sql." WHERE ".$where.$limit.$orderby;
 	$pdo = $this->getConn()->query($sql);
 	$pdo->setFetchMode(PDO::FETCH_ASSOC);
 	return $pdo->fetchAll();
@@ -130,15 +135,15 @@ public function Read($where = '', $table = ''){
 public function Update($id,$camp = 'id',$data,$table){
 	$tab = trim($table) == '' ? $this->table : $table;
 	$dt =  $this->getStr($data);
-	$sql = 'UPDATE '.$tab.' SET '.$dt['cmb'].' WHERE '.$camp.'='.$id;
+	$sql = "UPDATE ".$tab." SET ".$dt['cmb']." WHERE ".$camp."='".$id."'";
 	$pdo = $this->getConn()->prepare($sql);
 	$this->prepareSql($pdo,$data,$dt);
 	}
 	
 public function Delete($id, $camp='id', $table = ''){
 	$tab = trim($table) == '' ? $this->table : $table;
-	$sql = 'DELETE FROM '.$tab.' WHERE '.$camp.'='.$id;
+	$sql = "DELETE FROM ".$tab." WHERE ".$camp."='".$id."'";
 	$pdo = $this->getConn()->query($sql);
 	}	
-
-}//Fecha a classe de banco de dados
+	
+}
